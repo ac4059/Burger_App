023dd7eb13bd18422c79d0c9e80b451824376e01
Arjun Roy <arjunroychoudhry4059@gmail.com>
2020-07-08 16:32:24 +0100

    refactoring of burger app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
- import logo from './logo.svg';
- import './App.css';
- import BurgerBuilder from './Containers/BurgerBuilder/burgerBuilder';
+import React from 'react';
+import Layout from './Components/Layout/layout';
+import logo from './logo.svg';
+import './App.css';
 
- class App extends Component {
+class App extends Component {
    render() {
      return (
        <div>
        <Layout>
-         <BurgerBuilder />
+           <p>Test</p>
          </Layout>
-       </div>
-     );
-   }
- }
+         </div>
+        );
+      }
+    }
 
- export default App;
+      export default App;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
